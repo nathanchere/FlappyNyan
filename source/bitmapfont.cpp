6af2ea64a3a197bbdad4442547b43c506c4c73a2
Nathan Chere <git@nathanchere.com.au>
2014-02-16 18:56:35 +1100

    Bitap font class

---
+++ source/bitmapfont.cpp
@@ -0,0 +1,87 @@
+#include <nds.h>
+#include <stdio.h>
+#include <gl2d.h>
+#include "h/bitmapfont.h"
+
+BitmapFont::BitmapFont(){ }
+
+int BitmapFont::Load( glImage              *_font_sprite,
+				   const unsigned int   numframes, 
+				   const unsigned int   *texcoords,
+				   GL_TEXTURE_TYPE_ENUM type,
+				   int 	              	sizeX,
+				   int 	              	sizeY,
+				   int 	              	param,
+				   int					pallette_width,
+				   const u16			*palette,
+				   const uint8          *texture	 
+				 )
+
+{
+	font_sprite = _font_sprite;
+	
+	int textureID = 
+		glLoadSpriteSet( font_sprite,
+						 numframes, 
+						 texcoords,
+						 type,
+						 sizeX,
+						 sizeY,
+						 param,
+						 pallette_width,
+					     palette,
+						 texture	 
+					   );
+					   
+	return textureID;
+
+}
+
+void BitmapFont::Print( int x, int y, const char *text )
+{
+	unsigned char font_char;
+	
+	while( *text )
+	{
+		font_char = ( *(unsigned char*)text++ ) - 32;
+		glSprite( x, y, GL_FLIP_NONE, &font_sprite[font_char] );
+		x += font_sprite[font_char].width; 
+	}
+	
+}
+
+void BitmapFont::Print( int x, int y, int value )
+{	
+	sprintf( str,"%i",value );
+	Print( x, y, str );
+}
+
+void BitmapFont::PrintCentered( int x, int y, const char *text )
+{
+
+	unsigned char font_char;
+	int total_width = 0;
+	char *o_text = (char*)text;
+	
+	while( *text )
+	{
+		font_char = ( *(unsigned char*)text++ ) - 32;
+		total_width += font_sprite[font_char].width; 
+	}
+	
+	x = (SCREEN_WIDTH - total_width) / 2; 
+	
+	text = o_text;
+	while( *text )
+	{
+		font_char = (*(unsigned char*)text++) - 32;
+		glSprite( x, y, GL_FLIP_NONE, &font_sprite[font_char] );
+		x += font_sprite[font_char].width; 
+	}
+}
+
+void BitmapFont::PrintCentered( int x, int y, int value )
+{
+	sprintf( str,"%i",value );
+	PrintCentered( x, y, str );	
+}
